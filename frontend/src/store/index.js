45782ddc987ed92c36b59643e3b1e35a91ad2562
Rayan terki <2733003@collegelacite.ca>
2026-03-24 23:18:52 -0400

    brancher les routes users et roles

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import laboratoriesReducer from '../features/laboratories/laboratoriesSlice';
 import equipmentReducer from '../features/equipment/equipmentSlice';
 import authReducer from '../features/auth/authSlice';
+import usersReducer from '../features/users/usersSlice';
+import rolesReducer from '../features/roles/rolesSlice';
 
 export const store = configureStore({
   reducer: {
     laboratories: laboratoriesReducer,
     equipment: equipmentReducer,
     auth: authReducer,
+    users: usersReducer,
+    roles: rolesReducer,
   },
 });
